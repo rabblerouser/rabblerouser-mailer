1677b9668e4264d59d3d440fc49145a274439187
Cam Jackson <camjackson89@gmail.com>
2017-02-02 14:12:24 +1100

    Set up project with express and rr-stream-client

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const createClient = require('rabblerouser-stream-client');
+
+const streamClientSettings = {
+  stream: 'TODO: Should not need this param',
+  eventAuthToken: process.env.EVENT_AUTH_TOKEN || 'secret',
+};
+
+const streamClient = createClient(streamClientSettings);
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/mail', streamClient.consumer);
+
+streamClient.consumer.on('member-registered', member => {
+  console.log('Sending mail to:', member.email);
+});
+
+app.listen(8080, () => {
+  console.log('Listening for events');
+});
