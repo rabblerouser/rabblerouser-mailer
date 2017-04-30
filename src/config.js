044688d2ebed352a6247f4dcbb5db6bd7a4347bc
Cam Jackson <camjackson89@gmail.com>
2017-05-01 08:43:27 +1000

    Stream client creation into its own module
    Also, add config for working with local dev streams

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   sesRegion: 'us-east-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  sesEndpoint: process.env.SES_ENDPOINT,
+  kinesisRegion: process.env.KINESIS_REGION || 'ap-southeast-2',
+  kinesisEndpoint: process.env.KINESIS_ENDPOINT,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'FAKE',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'FAKE',
+  streamName: process.env.STREAM_NAME,
   listenerAuthToken: process.env.LISTENER_AUTH_TOKEN || 'secret',
 
   emailFromAddress: process.env.EMAIL_FROM_ADDRESS,
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const createClient = require('@rabblerouser/stream-client');
 
-const config = require('./config');
 const sendRegistrationEmail = require('./sendRegistrationEmail')
+const streamClient = require('./streamClient');
 
 const app = express();
 app.use(bodyParser.json());
-
-const streamClientSettings = {
-  listenWithAuthToken: config.listenerAuthToken,
-};
-const streamClient = createClient(streamClientSettings);
 
 streamClient.on('member-registered', sendRegistrationEmail);
 app.post('/events', streamClient.listen());
