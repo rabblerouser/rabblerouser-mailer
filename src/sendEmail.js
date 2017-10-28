614552a5eb5b03e2e0dcf2b98d4300e8fa072e89
Cam Jackson <camjackson89@gmail.com>
2017-10-28 17:52:26 +0800

    Tweak the structure of the promise chains/error handling logic

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -23,6 +23,16 @@ const assembleEmail = (emailEvent) => {
   ));
 };
 
+const sendAllEmails = (sentRecipients, failedRecipients, to, id) => nodemailerParams => (
+  Promise.all(to.map(recipient => (
+    nodemailer.sendMail({ ...nodemailerParams, to: [recipient] })
+      .then(
+        () => sentRecipients.push(recipient) && logger.info(`Sent email ${id} to ${recipient}`),
+        err => failedRecipients.push(recipient) && logger.error(`Failed to send email ${id} to ${recipient} with error: ${err}`)
+      )
+  )))
+);
+
 const sendEmail = (emailEvent) => {
   const { to, id } = emailEvent;
 
@@ -30,20 +40,20 @@ const sendEmail = (emailEvent) => {
   const failedRecipients = [];
 
   return assembleEmail(emailEvent)
-    .then(nodemailerParams => (
-      Promise.all(to.map(recipient => (
-        nodemailer.sendMail({ ...nodemailerParams, to: [recipient] })
-          .then(
-            () => sentRecipients.push(recipient) && logger.info(`Sent email ${id} to ${recipient}`),
-            err => failedRecipients.push(recipient) && logger.error(`Failed to send email ${id} to ${recipient} with error: ${err}`)
-          )
-      )))
-    ))
+    .then(
+      sendAllEmails(sentRecipients, failedRecipients, to, id),
+      (err) => {
+        logger.error(`Failed to assemble email with id: ${id} with error ${err}`);
+        to.forEach(recipient => failedRecipients.push(recipient));
+      }
+    )
     .then(publishEmailEvent('email-sent', id, sentRecipients))
     .then(publishEmailEvent('email-failed', id, failedRecipients))
     .catch((err) => {
-      logger.error(`Failed to fetch email with id: ${id} from s3 with error ${err}`);
-      return publishEmailEvent('email-failed', id, to)();
+      // We have to swallow this error. If it bubbles up, the event-forwarder will send us the same
+      // event again, which might cause us to double-email people. If we ever get here, it's a bug.
+      logger.error('BIG ERROR! All errors should have been handled by this point, so this should never happen:');
+      logger.error(err);
     });
 };
 
